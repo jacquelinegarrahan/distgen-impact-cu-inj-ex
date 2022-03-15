75d28a4b8c6ee9c95680165b96586570969d286e
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-03-15 12:14:37 -0700

    Check in dashboard creation

--- distgen_impact_cu_inj_ex/flow/flow.py
+++ distgen_impact_cu_inj_ex/flow/flow.py
@@ -1,19 +1,43 @@
-from distgen_impact_cu_inj_ex.utils import write_distgen_xy_dist, isolate_image
-from distgen_impact_cu_inj_ex.model import ImpactModel, DistgenModel, LUMEConfiguration
 from impact.impact_distgen import archive_impact_with_distgen
+from slac_services.services.scheduling import MongoDBResult
+from slac_services import service_container
 import numpy as np
+import os
 from prefect import Flow, task, Parameter
 from prefect.storage import Docker
 from distgen import Generator
 from prefect.run_configs import KubernetesRun
-import os
+from prefect.engine.results import LocalResult
+from distgen_impact_cu_inj_ex import dashboard
+from distgen_impact_cu_inj_ex.utils import write_distgen_xy_dist, isolate_image
+from distgen_impact_cu_inj_ex.model import ImpactModel, DistgenModel, LUMEConfiguration
+from distgen_impact_cu_inj_ex.dashboard import make_dashboard
+
 from distgen_impact_cu_inj_ex import (
     CU_INJ_MAPPING_TABLE,
     IMPACT_INPUT_VARIABLES,
-    IMPACT_OUTPUT_VARIABLES,
     DISTGEN_INPUT_VARIABLES,
-    DISTGEN_OUTPUT_VARIABLES,
 )
+
+
+
+# DEFINE PARAMETERS GLOBALLY FOR USE IN DYNAMIC RESULT STORAGE STRINGS
+distgen_input_filename = Parameter("distgen_input_filename")
+distgen_output_filename = Parameter("distgen_output_filename")
+distgen_settings = Parameter("distgen_settings")
+distgen_configuration = Parameter("distgen_configuration")
+distgen_pv_values = Parameter("distgen_pv_values")
+distgen_pvname_to_input_map = Parameter("distgen_pvname_to_input_map")
+impact_configuration = Parameter("impact_configuration")
+impact_settings = Parameter("impact_settings")
+impact_pv_values = Parameter("impact_pv_values")
+impact_pvname_to_input_map = Parameter("impact_pvname_to_input_map")
+impact_archive_file = Parameter("impact_archive_file")
+archive_dir = Parameter("impact_archive_dir")
+#  summary_dir = Parameter("impact_summary_dir")
+pv_collection_isotime = Parameter("pv_collection_isotime")
+dashboard_dir = Parameter("dashboard_dir")
+
 
 
 @task
@@ -119,21 +143,67 @@ def run_impact(
 
     return (model, output_variables)
 
-
 @task
-def archive(distgen_output, impact_output, archive_dir, model_id, pv_collection_isotime):
+def archive(distgen_output, impact_output, archive_dir, pv_collection_isotime):
     # get fingerprint
     distgen_model = distgen_output[0]
     impact_model = impact_output[0]
  #   fingerprint = fingerprint_impact_with_distgen(impact_model.I, distgen_model.G)
 
-    archive_file = os.path.join(archive_dir, f"{str(model_id)}_{pv_collection_isotime}.h5")
+    archive_file = os.path.join(archive_dir, f"{os.environ('PREFECT__CONTEXT__FLOW_ID')}_{pv_collection_isotime}.h5")
 
     assert os.path.exists(archive_dir), f'archive dir does not exist: {archive_dir}'
 
     archive_impact_with_distgen(impact_model.I, distgen_model.G, archive_file)
 
     return archive_file    
+
+
+
+@task
+def create_dashboard(pv_collection_isotime, dashboard_dir, impact_output):
+
+    DASHBOARD_KWARGS = {'outpath': dashboard_dir,
+                'screen1': 'YAG02',
+                'screen2': 'YAG03',
+                'screen3': 'OTR2',
+                'ylim' : (0, 2e-6), # Emittance scale                        
+                'name' : f"{os.environ('PREFECT__CONTEXT__FLOW_ID')}_{pv_collection_isotime}"
+                }
+
+    plot_file = make_dashboard(impact_output[0], itime=pv_collection_isotime, **DASHBOARD_KWARGS)
+
+    return plot_file
+
+
+@task(result=MongoDBResult(model_type="impact", results_db=service_container.results_db()))
+def store_results(pv_collection_isotime, impact_settings, impact_input_variables, impact_configuration, impact_output, dashboard_file):
+
+    impact_settings.update(
+        {var_name: var.value for var_name, var in impact_input_variables}
+    )
+
+
+    # output vars second item in tuple
+    impact_outputs = {var.name: var.value for var in impact_output[1]}
+
+
+    dat = {
+        "flow_id": os.environ("PREFECT__CONTEXT__FLOW_ID"),
+        "isotime": pv_collection_isotime,
+        "inputs": impact_settings, 
+        "config": impact_configuration,
+        "outputs": impact_outputs,
+        "plot_file": dashboard_file
+    }
+    return dat
+
+
+
+
+
+
+
         
 
 """
@@ -198,23 +268,6 @@ with Flow(
     ),
 ) as flow:
 
-    distgen_input_filename = Parameter("distgen_input_filename")
-    distgen_output_filename = Parameter("distgen_output_filename")
-    distgen_settings = Parameter("distgen_settings")
-    distgen_configuration = Parameter("distgen_configuration")
-    distgen_pv_values = Parameter("distgen_pv_values")
-    distgen_pvname_to_input_map = Parameter("distgen_pvname_to_input_map")
-    impact_configuration = Parameter("impact_configuration")
-    impact_settings = Parameter("impact_settings")
-    impact_pv_values = Parameter("impact_pv_values")
-    impact_pvname_to_input_map = Parameter("impact_pvname_to_input_map")
-    impact_archive_file = Parameter("impact_archive_file")
-    archive_dir = Parameter("impact_archive_dir")
-    model_id = Parameter("model_id")
-  #  summary_dir = Parameter("impact_summary_dir")
-    pv_collection_isotime = Parameter("pv_collection_isotime")
-
-
     distgen_input_variables = format_distgen_epics_input(distgen_pv_values, distgen_pvname_to_input_map)
 
     distgen_output = run_distgen(
@@ -236,7 +289,14 @@ with Flow(
         distgen_output
     )
 
-    archive_file = archive(distgen_output, impact_output, archive_dir, model_id, pv_collection_isotime)
+
+    dashboard_file = create_dashboard(pv_collection_isotime, dashboard_dir, impact_output)
+    archive_file = archive(distgen_output, impact_output, archive_dir, pv_collection_isotime)
+    store_results(pv_collection_isotime, impact_settings, impact_input_variables, impact_configuration, impact_output, dashboard_file)
+
+
+
+
 
   #  summary_file = summarize_impact(impact_settings, impact_configuration, impact_pv_values, impact_output, pv_collection_isotime, impact_model_name, summary_dir)
 
